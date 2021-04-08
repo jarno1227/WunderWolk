8e2f8e8b5a1a42564d1301bd9b72c680c7e3e771
BartvanS <bartvansevenbergen@hotmail.com>
2021-04-08 11:56:35 +0200

    api connector classes v1
    needs small improvements but the basics are functional

--- RPI/modules/program.py
+++ RPI/modules/program.py
@@ -6,7 +6,8 @@ class Program:
         # todo: get latest settings from file or database or something
         self.settings = Settings(location=[51.57046107093778, 5.050113150625251])
         # self.API = Api()
-        self.SocialConnect = SocialConnect("aapje", settings=self.settings)
+        self.SocialConnect = SocialConnect("bddae9b9df86095e0d4b9908a7a9b622", settings=self.settings)
         self.WeatherConnect = WeatherConnect("f71af11b8e02b30c2ed988487f0dd533", settings=self.settings)
-        self.WeatherConnect.fetch_data()
+        self.WeatherConnect.fetch_hourly()
+        # self.SocialConnect.fetch_data()
         # self.MQTT = MQTT()
