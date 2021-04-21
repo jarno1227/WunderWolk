6c1aca4ed046f11148a9470c178078b9d422ef9e
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-04-21 15:04:48 +0200

    test thunder

--- RPI/tests/hardware_test.py
+++ RPI/tests/hardware_test.py
@@ -1,8 +1,13 @@
 from modules.hardware import *
-
-# h = Hardware()
+import time
+h = Hardware()
 
 
 def test_arduino_map():
     val = 1023
     assert arduino_map(val, 0, 1023, 0, 255) == 255
+
+def test_thunder():
+    h.make_thunder()
+    while(True):
+        print("kaas")
